e62551d1ddc3b4eea3308a4ee3ab0fe4886ad0ce
arun.menon <ntarunmenon@gmail.com>
2016-02-10 09:31:33 +0400

    Commit First Cut

---
+++ angular-01/WebContent/MainController.js
@@ -0,0 +1,14 @@
+function MainController($scope) {
+
+  'use strict';
+ 
+  $scope.users = [
+                  {
+                	  name:'Arun Menon',
+                	  description:'Mine'
+                    },
+                    {
+                    	name:'Sherlock',
+                  	  	description:'Sherlocks'
+                    }];
+}
