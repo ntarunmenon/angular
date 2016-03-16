c0ee1222c4d8be7dec1fa53d5d8227b9c51077d1
arun.menon <ntarunmenon@gmail.com>
2016-03-16 11:44:50 +0400

    Adding Changes

--- angularjs-trakt/WebContent/js/tv-premieres-app/controllers/mainController.js
+++ angularjs-trakt/WebContent/js/tv-premieres-app/controllers/mainController.js
@@ -1,7 +1,10 @@
 app.controller("mainController", function($scope, $http){
 
 	 $scope.apiKey = "8c6812fed77d27d9d83063ed4cd8c28b15fa0cb79fffc0f405ac57bc57ab1b68";
+	 $scope.filterText = null;
 	 $scope.results = [];
+	 $scope.availableGenres = [];
+     $scope.genreFilter = null;
 	 $scope.init = function() {
     	 //API requires a start date
         var today = new Date();
@@ -16,6 +19,19 @@ app.controller("mainController", function($scope, $http){
         }).then(function mySucces(response) {
         	angular.forEach(response.data, function(tvshow, key) {
 			    $scope.results.push(tvshow);
+			    
+			    angular.forEach(tvshow.show.genres, function(genre, index){
+                    var exists = false;
+                    angular.forEach($scope.availableGenres, function(avGenre, index){
+                        if (avGenre == genre) {
+                            exists = true;
+                        }
+                    });
+                    if (exists === false) {
+                        $scope.availableGenres.push(genre);
+                    }
+                });
+			    
         	}) 
         	console.log('data is' , response.data);
         }, function myError(response) {
@@ -23,10 +39,33 @@ app.controller("mainController", function($scope, $http){
         });
     };
 
+    $scope.setGenreFilter = function(genre) {
+        $scope.genreFilter = genre;
+    }
+    
+    
 });
 
 app.config(['$httpProvider', function($httpProvider) {
 	console.log('delete header');
 	delete $httpProvider.defaults.headers.common["X-Requested-With"]
 }])
+
+app.filter('isGenre', function() {
+        return function(input, genre) {
+            if (typeof genre == 'undefined' || genre == null) {
+                return input;
+            } else {
+                var out = [];
+                for (var a = 0; a < input.length; a++){
+                    for (var b = 0; b < input[a].show.genres.length; b++){
+                        if(input[a].show.genres[b] == genre) {
+                            out.push(input[a]);
+                        }
+                    }
+                }
+                return out;
+            }
+        };
+    });
 
